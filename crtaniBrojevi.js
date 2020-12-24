6b972608e115985234637a6beef1a5ea73b0ea3f
ivandeka <vitezkoja@yopmail.com>
2020-12-24 17:24:23 +0100

    v0.8 - refaktoring i nove sitnice

--- crtaniBrojevi.js
+++ crtaniBrojevi.js
@@ -1,4 +1,10 @@
 const crtaniBrojevi = {
+	0: [
+		"src/0/1529489866.svg",
+		"src/0/cibo00-Cibo00-Number-0.svg",
+		"src/0/Monster Number 10.svg",
+		"src/0/kablam-Number-Animals-10.svg"
+],
 	1: [
 		"src/1/kablam-Number-Animals-1.svg",
 		"src/1/Monster Number 1.svg",
@@ -9,41 +15,24 @@ const crtaniBrojevi = {
 		"src/1/1529489873.svg",
 		"src/1/Gerald_G_Creation_Days_Numbers.svg"
 ],
-	2: [
-		"src/2/1529489882.svg",
-		"src/2/Monster Number 2.svg",
-		"src/2/number-2.svg",
-		"src/2/1459288631.svg",
-		"src/2/Gerald_G_Creation_Days_Numbers_1.svg",
-		"src/2/two_outline.svg"
+	9: [
+		"src/9/1529489945.svg",
+		"src/9/Monster Number 9.svg",
+		"src/9/kablam-Number-Animals-8.svg",
+		"src/9/number-9.svg"
 ],
-	3: [
-		"src/3/number-3.svg",
-		"src/3/1529489889.svg",
-		"src/3/Gerald_G_Creation_Days_Numbers_2.svg",
-		"src/3/kablam-Number-Animals-2.svg",
-		"src/3/Monster Number 3.svg",
-		"src/3/3-THREE.svg",
-		"src/3/1459288517.svg",
-		"src/3/1278473724.svg"
+	8: [
+		"src/8/1529489936.svg",
+		"src/8/Monster Number 8.svg",
+		"src/8/kablam-Number-Animals-7.svg",
+		"src/8/number-8.svg"
 ],
-	4: [
-		"src/4/cibo00-Cibo00-Number-4.svg",
-		"src/4/Monster Number 4.svg",
-		"src/4/Gerald_G_Creation_Days_Numbers_3.svg",
-		"src/4/number-4.svg",
-		"src/4/scared-4.svg",
-		"src/4/1529489903.svg",
-		"src/4/kablam-Number-Animals-3.svg"
-],
-	5: [
-		"src/5/five.svg",
-		"src/5/cibo00-Cibo00-Number-5.svg",
-		"src/5/Monster Number 5.svg",
-		"src/5/1529489911.svg",
-		"src/5/1459289068.svg",
-		"src/5/Gerald_G_Creation_Days_Numbers_4.svg",
-		"src/5/number-5.svg"
+	7: [
+		"src/7/kablam-Number-Animals-6.svg",
+		"src/7/1414080898.svg",
+		"src/7/1529489927.svg",
+		"src/7/Gerald-G-Creation-Days-Numbers-6.svg",
+		"src/7/Monster Number 7.svg"
 ],
 	6: [
 		"src/6/happy-6.svg",
@@ -58,29 +47,40 @@ const crtaniBrojevi = {
 		"src/6/1459289145.svg",
 		"src/6/six-outline.svg"
 ],
-	7: [
-		"src/7/kablam-Number-Animals-6.svg",
-		"src/7/1414080898.svg",
-		"src/7/1529489927.svg",
-		"src/7/Gerald-G-Creation-Days-Numbers-6.svg",
-		"src/7/Monster Number 7.svg"
+	5: [
+		"src/5/five.svg",
+		"src/5/cibo00-Cibo00-Number-5.svg",
+		"src/5/Monster Number 5.svg",
+		"src/5/1529489911.svg",
+		"src/5/1459289068.svg",
+		"src/5/Gerald_G_Creation_Days_Numbers_4.svg",
+		"src/5/number-5.svg"
 ],
-	8: [
-		"src/8/1529489936.svg",
-		"src/8/Monster Number 8.svg",
-		"src/8/kablam-Number-Animals-7.svg",
-		"src/8/number-8.svg"
+	4: [
+		"src/4/cibo00-Cibo00-Number-4.svg",
+		"src/4/Monster Number 4.svg",
+		"src/4/Gerald_G_Creation_Days_Numbers_3.svg",
+		"src/4/number-4.svg",
+		"src/4/scared-4.svg",
+		"src/4/1529489903.svg",
+		"src/4/kablam-Number-Animals-3.svg"
 ],
-	9: [
-		"src/9/1529489945.svg",
-		"src/9/Monster Number 9.svg",
-		"src/9/kablam-Number-Animals-8.svg",
-		"src/9/number-9.svg"
+	3: [
+		"src/3/number-3.svg",
+		"src/3/1529489889.svg",
+		"src/3/Gerald_G_Creation_Days_Numbers_2.svg",
+		"src/3/kablam-Number-Animals-2.svg",
+		"src/3/Monster Number 3.svg",
+		"src/3/3-THREE.svg",
+		"src/3/1459288517.svg",
+		"src/3/1278473724.svg"
 ],
-	0: [
-		"src/0/1529489866.svg",
-		"src/0/cibo00-Cibo00-Number-0.svg",
-		"src/0/Monster Number 10.svg",
-		"src/0/kablam-Number-Animals-10.svg"
+	2: [
+		"src/2/1529489882.svg",
+		"src/2/Monster Number 2.svg",
+		"src/2/number-2.svg",
+		"src/2/1459288631.svg",
+		"src/2/Gerald_G_Creation_Days_Numbers_1.svg",
+		"src/2/two_outline.svg"
 ]
 }
